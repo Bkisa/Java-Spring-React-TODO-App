2d30e48128a42072c4fdee1c7683fe648842e028
Buğrahan KISA <48793621+Bkisa@users.noreply.github.com>
2024-08-05 03:39:32 +0300

    changes

---
+++ todoapp/TodoAppRepositoryLib/src/test/java/org/bugrahan/todoapp/repository/TodoRepositoryFindAllNotCompletedTest.java
@@ -0,0 +1,28 @@
+package org.bugrahan.todoapp.repository;
+
+import org.bugrahan.todoapp.constant.TestConstant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.stream.StreamSupport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@TestPropertySource(locations = TestConstant.UNITTEST_PROPS_FILE)
+public class TodoRepositoryFindAllCompletedTest {
+    @Autowired
+    private ITodoRepository m_todoRepository;
+
+    @Test
+    public void test()
+    {
+        var expectedCount = 11L;
+        var count = StreamSupport.stream(m_todoRepository.findAllCompleted().spliterator(), false).count();
+
+        assertEquals(expectedCount, count);
+    }
+}
