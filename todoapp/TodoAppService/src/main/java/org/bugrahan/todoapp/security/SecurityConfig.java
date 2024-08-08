a93a40cb18fd2262f4cf673549bc696f82874468
Buğrahan KISA <48793621+Bkisa@users.noreply.github.com>
2024-08-08 21:15:28 +0300

    security package is added and SecurityConfig file move to this package

--- todoapp/TodoAppService/src/main/java/org/bugrahan/todoapp/security/SecurityConfig.java
+++ todoapp/TodoAppService/src/main/java/org/bugrahan/todoapp/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.bugrahan.todoapp.webconfig;
+package org.bugrahan.todoapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
